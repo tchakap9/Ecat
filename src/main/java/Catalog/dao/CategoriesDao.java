3256a6c237936342f15cc60d9f5c6f487cb47f69
tchakap9 <dylan.nakong-ngongang@telecomnancy.net>
2021-04-13 01:26:37 +0200

    updating

--- src/main/java/Catalog/dao/CategoriesDao.java
+++ src/main/java/Catalog/dao/CategoriesDao.java
@@ -1,4 +1,34 @@
 package Catalog.dao;
 
+import Catalog.model.Categories;
+
+import java.sql.*;
+import java.util.Date;
+
 public class CategoriesDao {
+
+    public String url;
+
+    public  CategoriesDao(String url){
+        this.url = url;
+    }
+    public Categories getCategoriesbyId(int id) {
+        try {
+            Connection con = DriverManager.getConnection(url);
+            PreparedStatement statement = con.prepareStatement
+                    ("select * from Categories where id = ?");
+            ResultSet resultSet = statement.executeQuery();
+            Categories categories = new Categories();
+            while (resultSet.next()){
+              /*  categories.setId(resultSet.getInt("Id"));
+                categories.setParentId(resultSet.getInt("ParentId"));
+                categories.setLevel(resultSet.getInt("Level")*/
+            }
+            return categories;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
 }
--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,2 +1,13 @@
+import Catalog.dao.CategoriesDao;
+import Catalog.model.Categories;
+
 public class MainApp {
+    public static void main(String[] args){
+        System.out.println("Starting");
+        String url ="jdbc:odbc:ecat.db";
+        CategoriesDao categoriesDao = new CategoriesDao(url);
+
+        Categories categories = categoriesDao.getCategoriesbyId(4936);
+        System.out.println("Done");
+    }
 }
