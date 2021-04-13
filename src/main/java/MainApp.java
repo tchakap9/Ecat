9f4f33c9a5cf2da712c0db0686477dd878420a89
Jordan Deudjui <jordandeudjui@gmail.com>
2021-04-14 01:37:59 +0200

    CategorieNames

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -4,7 +4,7 @@ import Catalog.model.Categories;
 public class MainApp {
     public static void main(String[] args){
         System.out.println("Starting");
-        String url ="jdbc:odbc:ecat.db";
+        String url ="jdbc:sqlite:src/ecat.db";
         CategoriesDao categoriesDao = new CategoriesDao(url);
 
         Categories categories = categoriesDao.getCategoriesbyId(4936);
