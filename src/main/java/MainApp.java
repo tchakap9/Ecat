d17745ec2499af2b8b2304d5c3af365396f5e29c
Jordan Deudjui <jordandeudjui@gmail.com>
2021-04-15 21:47:59 +0200

    Ajout du DRIVER jdbc

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -2,6 +2,7 @@ import Catalog.dao.CategoriesDao;
 import Catalog.model.Categories;
 
 public class MainApp {
+
     public static void main(String[] args){
         System.out.println("Starting");
         String url ="jdbc:sqlite:src/ecat.db";
