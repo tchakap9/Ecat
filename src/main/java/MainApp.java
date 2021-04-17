3345d28306bfd51a6de60491c39079f3cc2432f8
Jordan Deudjui <jordandeudjui@gmail.com>
2021-04-17 18:08:06 +0200

    Reference DAO

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,5 +1,7 @@
 import Catalog.dao.CategoriesDao;
+import Catalog.dao.ReferenceDao;
 import Catalog.model.Categories;
+import Catalog.model.ProductDescriptions;
 
 import java.text.SimpleDateFormat;
 
@@ -9,14 +11,20 @@ public class MainApp {
         System.out.println("Starting");
         String url ="jdbc:sqlite:src/ecat.db";
         CategoriesDao categoriesDao = new CategoriesDao(url);
-        java.util.Scanner entree =new java.util.Scanner (System.in);
+        ReferenceDao ReferenceDao = new ReferenceDao(url);
+        java.util.Scanner entree = new java.util.Scanner (System.in);
         System.out.println("================================");
         System.out.println("Bienvenue Dans Notre Application");
         System.out.println("================================");
-        System.out.println("Entrez Le nom de la Catégories de votre choix");
+        System.out.println("Entrez Le nom de la Catégories de votre choix :");
         String nom = entree.nextLine();
+        System.out.println("Entrez La reférence du produit :");
+        int Reference = entree.nextInt();
         System.out.println(nom);
+        System.out.println(Reference);
         Categories categories = categoriesDao.getCategoriesbyId(nom);
         System.out.println(categories.Image);
+        ProductDescriptions ProductDescriptions = ReferenceDao.getProductDescriptionsbyId(Reference);
+        System.out.println(ProductDescriptions.Value);
     }
 }
